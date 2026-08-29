ad07d3b275c37fbd869f7124b9c77749d30054cd
contact-holding <heritagesarl8790@gmail.com>
2026-08-29 07:05:18 +0100

    buikd errur

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "etmatyqawktbyaezzcks.supabase.co",
-        pathname: "/storage/v1/object/public/sites/**",
-      },
-    ],
-
-
-
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
